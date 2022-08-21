6993cd3d75b7d2049b6d7b52c7aede55dfbdcd12
CJINBNH <alex_egorov_89@mail.ru>
2022-08-21 21:08:17 +0400

    Написали функцию для вычисления факториала

--- examples/example_003/Program.cs
+++ examples/example_003/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вычисление факториала с помощью функции
+
+Console.Clear();
+double Factorial(int n)
+{
+    if (n == 1) return 1; // 1! = 1; 0! = 1
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
