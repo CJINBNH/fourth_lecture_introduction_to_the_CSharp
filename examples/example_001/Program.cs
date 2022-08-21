00e02ccd6a287cd193c5c90a90ac7914e98c4edc
CJINBNH <alex_egorov_89@mail.ru>
2022-08-21 11:31:13 +0400

    Записали двумерные массивы с помощью метода и автозаполнения

--- examples/example_001/Program.cs
+++ examples/example_001/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Двумерные массивы:
+// string[,] table = new string[2,3];
+//int[,] matrix = new int[5,8] - 5 указывает на количество строк, 8 - количество столбцов
+Console.Clear();
+// String.Empty
+// table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+// table[1,0] table[0,1] table[1,2] table[1,3] table[1,4]
+// string[,] table = new string[2, 5];
+// table[1, 2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.Write($"-{table[rows, columns]} -");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[,] matrix = new int[3, 4];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i,j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Описываем метод выводящий заполненную таблицу на экран
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine("");
+FillArray(matrix);
+PrintArray(matrix);
